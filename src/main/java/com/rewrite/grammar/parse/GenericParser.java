29d22e39e9cfc08f9eb2afef2114483c18382cd8
Kevin Kerr <kkerr@triplelift.com>
2019-12-24 15:32:08 -0500

    making progress

--- src/main/java/com/rewrite/grammar/parse/GenericParser.java
+++ src/main/java/com/rewrite/grammar/parse/GenericParser.java
@@ -6,20 +6,51 @@ import com.rewrite.grammar.parse.Tokenizer.Token.TokenType;
 
 public class GenericParser {
 	private static class GenericParserTuple {
-		boolean required;
-		GenericParserTuple next;
-		GenericParser parser;
+		boolean repeated;
+		private GenericParserTuple next;
+		private GenericParser parser;
+
+		public GenericParserTuple(GenericParser gp, boolean repeated) {
+			this.repeated = repeated;
+			this.parser = gp;
+		}
 	}
 
-	private GenericParserTuple head;
-	private GenericParserTuple current;
+	private GenericParserTuple head = new GenericParserTuple(new NamedStubParser("head"), false);
+	private GenericParserTuple current = head;
 
-	public static GenericParser from(String name, TokenStream tokens) {
+	public static GenericParser from(String name, TokenStream tokens) throws Exception {
 		GenericParser ret = new GenericParser();
 		Token t = tokens.getNext();
 		while (t.getType() != TokenType.TT_PERIOD) {
+			if (t.getType() == TokenType.TT_LCURLY) {
+				// repeated field
+				t = tokens.getNext();
+				GenericParser gp = fromToken(t, tokens);
+				GenericParserTuple tuple = new GenericParserTuple(gp, true);
+				ret.current.next = tuple;
+				t = tokens.getNext();
+				if (t.getType() != TokenType.TT_RCURLY) {
+					throw new Exception();
+				}
+			}
 
 		}
+		return ret;
+	}
+
+	private static GenericParser fromToken(Token t, TokenStream tokens) throws Exception {
+		if (t.getType() == TokenType.TT_VAR) {
+			return new NamedStubParser(t.getLit());
+		} else if (t.getType() == TokenType.TT_SINGLE_QUOTE) {
+			t = tokens.getNext();
+			String quotedValue = "";
+			while (t.getType() != TokenType.TT_SINGLE_QUOTE) {
+				quotedValue += t.getLit();
+			}
+			return new SimpleStringParser(quotedValue);
+		}
+		throw new Exception();
 	}
 
 	public boolean accepts(String str) {
--- src/main/java/com/rewrite/grammar/parse/SimpleStringParser.java
+++ src/main/java/com/rewrite/grammar/parse/SimpleStringParser.java
@@ -2,4 +2,8 @@ package com.rewrite.grammar.parse;
 
 public class SimpleStringParser extends GenericParser {
 
+	public SimpleStringParser(String quotedValue) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
