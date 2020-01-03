56aa54d72affdcf3bf20fca5dfd15dacb5db2f8a
Kevin Kerr <kkerr@triplelift.com>
2020-01-03 16:37:22 -0500

    fix the negative parser

--- src/main/java/com/rewrite/grammar/parse/NegativeParser.java
+++ src/main/java/com/rewrite/grammar/parse/NegativeParser.java
@@ -1,5 +1,7 @@
 package com.rewrite.grammar.parse;
 
+import java.util.Map;
+
 public class NegativeParser extends GenericParser {
 	private final GenericParser gp;
 
@@ -11,6 +13,10 @@ public class NegativeParser extends GenericParser {
 	public boolean accepts(StringPump sp) {
 		int n = sp.getIndex();
 		boolean b = !gp.accepts(sp);
+		if (!b) {
+			sp.setIndex(n);
+			return b;
+		}
 		sp.setIndex(n);
 		return b;
 	}
@@ -18,5 +24,10 @@ public class NegativeParser extends GenericParser {
 	@Override
 	public SyntaxNode parse(StringPump sp) {
 		return null;
+	}
+
+	@Override
+	public void unStub(Map<String, GenericParser> namedParsers) throws Exception {
+		gp.unStub(namedParsers);
 	}
 }
